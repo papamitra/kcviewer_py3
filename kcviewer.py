82cc991f3c2cda8d3eb16852ef535483f063847b
papamitra <papamitra@gmail.com>
2014-08-30 09:49:06 +0900

    use ignoreSslErrors

--- kcviewer.py
+++ kcviewer.py
@@ -2,7 +2,7 @@
 
 from PyQt5.QtCore import Qt, QUrl, QSettings
 from PyQt5.QtWidgets import (QAction, QApplication, QWidget)
-from PyQt5.QtNetwork import QNetworkProxy
+from PyQt5.QtNetwork import QNetworkProxy, QNetworkAccessManager
 from PyQt5.QtWebKitWidgets import QWebView
 from PyQt5 import QtWebKit, QtNetwork
 
@@ -10,16 +10,39 @@ from appdirs import AppDirs
 import simplejson
 import os
 
+class ProxyAccessManager(QNetworkAccessManager):
+    def __init__(self,parent):
+        super(ProxyAccessManager, self).__init__(parent)
+
+        print("NetworkAccessManager")
+        proxy = QNetworkProxy()
+        proxy.setType(QNetworkProxy.HttpProxy);
+        proxy.setHostName("localhost");
+        proxy.setPort(12345);
+        #QNetworkProxy.setApplicationProxy(proxy);
+        self.setProxy(proxy)
+
+        self.sslErrors.connect(self.handle_ssl_errors)
+
+    def handle_ssl_errors(self, reply, errors):
+        print("!!!!!!!!!! sslErrors")
+        for err in errors:
+            print(err)
+
+        reply.ignoreSslErrors()
+        return reply
+
 class KCView(QWidget):
     def __init__(self, url):
-        super(QWidget, self).__init__()
-
-        self.view = QWebView(self)
-        self.view.load(url)
-        self.view.show()
+        super(KCView, self).__init__()
 
         self.app_dirs = AppDirs("kcviwer", "papamitra")
 
+        self.view = QWebView(self)
+        am = ProxyAccessManager(self)
+        self.view.page().setNetworkAccessManager(am)
+
+        """
         # Load cookies
         user_data_dir = self.app_dirs.user_data_dir
         if not os.path.exists(user_data_dir):
@@ -39,6 +62,28 @@ class KCView(QWidget):
         web_setting.setAttribute(QtWebKit.QWebSettings.OfflineStorageDatabaseEnabled, True)
         web_setting.setAttribute(QtWebKit.QWebSettings.LocalStorageEnabled, True)
 
+        # Prepare cache dir
+        cache_path = self.app_dirs.user_cache_dir
+
+        local_storage_path = cache_path + '/LocalStorage'
+        if not os.path.exists(local_storage_path):
+            os.makedirs(local_storage_path)
+        web_setting.setLocalStoragePath(local_storage_path)
+
+        offlineStoragePath = cache_path + '/OfflineStorage'
+        if not os.path.exists(offlineStoragePath):
+            os.makedirs(offlineStoragePath)
+        QtWebKit.QWebSettings.setOfflineStoragePath(offlineStoragePath)
+
+        offlineWebApplicationCachePath = cache_path + '/OfflineWebApplication'
+        if not os.path.exists(offlineWebApplicationCachePath):
+            os.makedirs(offlineWebApplicationCachePath)
+        QtWebKit.QWebSettings.setOfflineWebApplicationCachePath(offlineWebApplicationCachePath)
+        """
+
+        self.view.load(url)
+        self.view.show()
+
     def put(self, value):
         key = 'cookieJar'
         self.cookie_store.setValue(key, simplejson.dumps(value))
@@ -54,21 +99,15 @@ class KCView(QWidget):
 def main():
     import sys
 
-    app = QApplication(sys.argv)
-
     if len(sys.argv) > 1:
         url = QUrl(sys.argv[1])
     else:
-        url = QUrl('http://www.google.com/ncr')
+        url = QUrl('https://google.com/')
+
+    app = QApplication(sys.argv)
 
     browser = KCView(url)
     browser.show()
-
-    proxy = QNetworkProxy()
-    proxy.setType(QNetworkProxy.HttpProxy);
-    proxy.setHostName("127.0.0.1");
-    proxy.setPort(8080);
-    QNetworkProxy.setApplicationProxy(proxy);
 
     sys.exit(app.exec_())
 
