dc3c4f189b2b1615816eb4e5afc8c43bb4bff6ea
papamitra <papamitra@gmail.com>
2014-09-06 12:16:11 +0900

    Add ApiThread that is parse and action for kscapi.

--- main.py
+++ main.py
@@ -8,23 +8,43 @@ import threading
 
 import kcproxy
 import kcviewer
+import Queue
 
-class ProxyThread(QThread):
-    receive_msg = pyqtSignal(QVariant)
+class ProxyThread(threading.Thread):
+    #receive_msg = pyqtSignal(QVariant)
 
     def __init__(self, parent=None):
         super(ProxyThread, self).__init__(parent)
         self.proxy = kcproxy.KCProxy(on_receive = self.on_receive)
 
     def run(self):
+        self.input_queue = Queue.Queue()
+        self.api_thread = ApiThread(self.input_queue)
+        self.api_thread.start()
         self.proxy.run()
 
     def on_receive(self, msg):
-        self.receive_msg.emit(msg)
+        #self.receive_msg.emit(msg)
+        pass
 
     def stop(self):
         self.proxy.shutdown()
-        self.wait()
+        self.input_queue.put(None)
+        self.api_thread.join()
+        self.join()
+
+class ApiThread(threading.Thread):
+    def __init__(self, intput_queue):
+        super(ApiThread, self).__init__()
+        self.input_queue = intput_queue
+
+    def run(self):
+        print('ApiThread start...')
+        while True:
+            d = self.input_queue.get()
+            if d is None:
+                break
+        print('ApiThread ...done')
 
 if __name__ == '__main__':
     import sys
@@ -41,7 +61,7 @@ if __name__ == '__main__':
 
     browser = kcviewer.KCView(url)
 
-    proxythread.receive_msg.connect(browser.on_receive)
+    #proxythread.receive_msg.connect(browser.on_receive)
 
     browser.show()
 
