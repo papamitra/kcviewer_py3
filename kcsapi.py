c8ca45ab5f39f74fc7ca452dae15b75b8b7d78a8
papamitra <papamitra@gmail.com>
2014-09-07 08:14:38 +0900

    implement insert into api_mst_ship

--- kcsapi.py
+++ kcsapi.py
@@ -6,7 +6,7 @@ import pickle
 import re
 import simplejson
 
-DEBUG_DB = "kscapi_debug.db"
+DEBUG_DB = 'kscapi_debug.db'
 CREATE_MESSAGE_TABLE = u"""
 create table if not exists msg(
   timestamp integer,
@@ -14,6 +14,19 @@ create table if not exists msg(
   data blob
 );
 """
+
+DATA_DB = 'data.db'
+CREATE_MASTER_SHIP_TABLE = u"""
+create table if not exists api_mst_ship(
+  api_id integer primary key,
+  api_name text not null
+);
+"""
+
+def get_cols(con, table_name):
+    cur = con.cursor()
+    cur.execute(u'select * from ' + table_name)
+    return [col[0] for col in cur.description]
 
 class ApiMessage(object):
     def __init__(self, path, json):
@@ -25,6 +38,11 @@ class KcsApi(object):
     def __init__(self):
         self.debug_con = sqlite3.connect(DEBUG_DB, isolation_level=None)
         self.debug_con.execute(CREATE_MESSAGE_TABLE)
+
+        self.con = sqlite3.connect(DATA_DB)
+        with self.con:
+            self.con.execute(CREATE_MASTER_SHIP_TABLE)
+            self.mst_ship_cols = get_cols(self.con, 'api_mst_ship')
 
     @staticmethod
     def parse_respose(msg):
@@ -45,9 +63,20 @@ class KcsApi(object):
         sql = u"insert into msg values (datetime('now'), ? , ?)"
         self.debug_con.execute(sql, (msg.path, sqlite3.Binary(pickle.dumps(msg.json))))
 
-    def dispatch(self, msg):
-        self.debug_out(msg)
+    def dispatch(self, msg, debug_out=True):
+        if debug_out:
+            self.debug_out(msg)
 
+        if msg.path == u'/kcsapi/api_start2':
+            try:
+                with self.con:
+                    records = msg.json['api_data']['api_mst_ship']
+                    self.con.executemany(u'insert into api_mst_ship (%s) values (%s)' % \
+                                         (','.join(self.mst_ship_cols),
+                                         ','.join(['?'] * len(self.mst_ship_cols))),
+                                         [[r[c] for c in self.mst_ship_cols] for r in records])
+            except Exception, e:
+                print("dispatch failed: " + str(e))
 
 # for debug
 def parse_debug_db(where = None):
