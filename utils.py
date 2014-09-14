9ef11cf6d499ab42a409cb8d75f22560e94fde1f
papamitra <papamitra@gmail.com>
2014-09-14 11:43:09 +0900

    utils.py: 接続時にcon.row_factory = sqlite3.Row. tablecols追加

--- utils.py
+++ utils.py
@@ -7,5 +7,10 @@ sqlite3.register_converter("IntList", lambda s: [int(i) for i in s.split(';')])
 
 def connect_db():
     con = sqlite3.connect('data.db', detect_types = sqlite3.PARSE_DECLTYPES)
+    con.row_factory = sqlite3.Row
     return con
 
+def tablecols(con, table_name):
+    cur = con.cursor()
+    cur.execute(u'select * from ' + table_name)
+    return [col[0] for col in cur.description]
