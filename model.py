718eeda453d89deb85adb319c4b1a6b346ed2179
papamitra <papamitra@gmail.com>
2014-09-14 16:52:23 +0900

    model.py: Add DeckPortInfo

--- model.py
+++ model.py
@@ -1,25 +1,37 @@
 
 import utils
 
-def impl_shipstatus(classname, base_types, dict):
-    def get_col(col):
-        return lambda self: self.row[col]
+class DbMapper(object):
+    def __init__(self, table_name):
+        self.table_name = table_name
 
-    con = utils.connect_db()
-    for col in utils.tablecols(con, 'ship_view'):
-        if col not in dict:
-            dict[col] = property(get_col(col))
+    def __call__(self, classname, base_types, dict):
+        def get_col(col):
+            return lambda self: self.row[col]
 
-    return type(classname, base_types, dict)
+        con = utils.connect_db()
+        for col in utils.tablecols(con, self.table_name):
+            if col not in dict:
+                dict[col] = property(get_col(col))
+
+        return type(classname, base_types, dict)
 
 class ShipStatus(object):
-    __metaclass__ = impl_shipstatus
-    def __init__(self, row):
-        self.row = row
-
-    @staticmethod
-    def new(con, ship_id):
+    __metaclass__ = DbMapper('ship_view')
+    def __init__(self, con, ship_id):
         cur = con.cursor()
         cur.execute(u'select * from ship_view where id=?', (ship_id,))
         row = cur.fetchone()
-        return ShipStatus(row)
+        self.row = row
+
+class DeckPortInfo(object):
+    __metaclass__ = DbMapper('api_deck_port')
+    def __init__(self, con, port_id):
+        self.con = con
+        cur = self.con.cursor()
+        cur.execute(u'select * from api_deck_port where api_id=?', (port_id,))
+        row = cur.fetchone()
+        self.row = row
+
+    def ships(self):
+        return [ShipStatus(self.con, ship_id) for ship_id in self.api_ship]
