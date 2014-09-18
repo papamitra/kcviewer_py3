27e6250d4eea78738433e48dfd8f808c7487b81a
papamitra <papamitra@gmail.com>
2014-09-18 22:18:14 +0900

    expedition_box.py: 表示実装

--- expedition_box.py
+++ expedition_box.py
@@ -3,14 +3,51 @@
 from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QFrame,
                              QSizePolicy, QLabel, QPushButton, QSpacerItem)
 
+import model
 import utils
+import time
 
 MAX_FLEET_NUM = 4
 
+class ExpeditionLabel(QLabel):
+    def __init__(self, parent, deck_no):
+        super(ExpeditionLabel, self).__init__(parent)
+        self.parent = parent
+        self.deck_no = deck_no
+        self.reload()
+
+    def reload(self):
+        con = self.parent.con
+        port = model.Port(con)
+        self.deck = port.deck(self.deck_no)
+        self.update()
+
+    @staticmethod
+    def formattime(sec):
+        h = sec/(60**2)
+        m = (sec % (60**2)) / 60
+        s = sec % (60**3)
+
+        return '{:02d}:{:02d}:{:02d}'.format(h,m,s)
+
+    def update(self):
+        prefix = u'/' + str(self.deck_no)
+        if self.deck is None:
+            self.setText(prefix + u'- 未開放')
+        elif self.deck.api_mission[2] == 0:
+            self.setText(prefix + u'- ')
+        else:
+            sec = self.deck.api_mission[2] - time.time() * 1000
+            if sec < 0:
+                sec = 0
+            self.setText(prefix + u'- ' + self.formattime(sec))
+
 class ExpeditionBox(QWidget):
     def __init__(self, parent=None):
-        super(ExpeditionBox, self).__init__()
-        
+        super(ExpeditionBox, self).__init__(parent)
+
+        self.con = utils.connect_db()
+
         self.vbox = QVBoxLayout()
         self.setLayout(self.vbox)
         self.label = QLabel()
@@ -23,7 +60,9 @@ class ExpeditionBox(QWidget):
         self.line.setFrameShadow(QFrame.Sunken)
         self.vbox.addWidget(self.line)
 
-        #for i in range(1, MAX_FLEET_NUM):
+        for i in range(1, MAX_FLEET_NUM):
+            label = ExpeditionLabel(self, i+1)
+            self.vbox.addWidget(label)
 
 if __name__ == '__main__':
     import sys
