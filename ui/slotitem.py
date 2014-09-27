df6a2bb3ba6d5ea3af3aa66b26646c8be7eaac87
papamitra <papamitra@gmail.com>
2014-09-27 14:22:44 +0900

    スロットアイコン追加

---
+++ ui/slotitem.py
@@ -0,0 +1,234 @@
+
+import xml.etree.ElementTree as ET
+
+from PyQt5.QtCore import QSize, Qt, QRectF, QRect
+from PyQt5.QtGui import (QBrush, QColor, QFont, QLinearGradient, QPainter,
+                         QPainterPath, QPalette, QPen, QTransform, QPolygonF)
+from PyQt5.QtWidgets import (QApplication, QComboBox, QGridLayout, QLabel,
+                             QSizePolicy, QSpinBox, QWidget, QHBoxLayout, QVBoxLayout,
+                             QGraphicsScene, QGraphicsView)
+
+from slotitem_parser import PathBuilder
+from math import sin, cos, pi
+
+ns = {'p' : "http://schemas.microsoft.com/winfx/2006/xaml/presentation",
+      'ed': "http://schemas.microsoft.com/expression/2010/drawing"}
+
+PRE_P = r"{http://schemas.microsoft.com/winfx/2006/xaml/presentation}"
+PRE_ED = r"{http://schemas.microsoft.com/expression/2010/drawing}"
+
+class MainWidget(QWidget):
+    def __init__(self):
+        super(MainWidget, self).__init__()
+
+        self.box = QVBoxLayout()
+        self.setLayout(self.box)
+
+        tree = ET.parse('SlotItemIcon.xml')
+        triggers = tree.findall(r'.//p:Trigger', namespaces=ns)
+
+        try:
+            i = 0
+            for trg in triggers:
+                if i == 0:
+                    hbox = QHBoxLayout()
+                    self.box.addLayout(hbox)
+                viewbox = trg.find('p:Setter/p:Setter.Value/p:ControlTemplate/p:Viewbox',namespaces=ns)
+                if viewbox is not None:
+                    grid = viewbox.find('p:Grid', namespaces=ns)
+                    if grid is not None:
+                        hbox.addWidget(IconBox([elm for elm in grid], self, grid))
+                else:
+                    path_elm = trg.find('p:Setter/p:Setter.Value/p:ControlTemplate/p:Path',namespaces=ns)
+                    if path_elm is not None:
+                        hbox.addWidget(IconBox([path_elm],self))
+                i += 1
+                i = i % 5
+        except None as e:
+            print(e)
+
+class IconBox(QWidget):
+    builder = PathBuilder()
+
+    def __init__(self, elms, parent, grid=None):
+        super(IconBox, self).__init__(parent)
+        self.elms = elms
+        self.setMinimumSize(QSize(50, 50))
+        self.grid = grid
+        self.whole_rect = None
+        self.scene = QGraphicsScene(0,0,50,50)
+
+        for elm in self.elms:
+            if elm.tag == PRE_P + 'Path':
+                self.create_path(elm)
+            elif elm.tag == PRE_P + 'Ellipse':
+                self.create_ellipse(elm)
+            elif elm.tag == PRE_ED + 'Arc':
+                self.create_arc(elm)
+            elif elm.tag == PRE_ED + 'RegularPolygon':
+                self.create_regular_polygon(elm)
+
+    def _margin(self, elm):
+        if 'Margin' in elm.attrib:
+            margin = [float(f) for f in elm.attrib['Margin'].split(',')]
+            if len(margin) == 1:
+                return margin * 4
+            return margin
+        return None
+
+    def _elm_wh(self, elm):
+        w = h = None
+        if 'Width' in elm.attrib:
+            w = float(elm.attrib['Width'])
+        elif self.grid is not None and 'Width' in self.grid.attrib:
+            w = float(self.grid.attrib['Width'])
+
+        if 'Height' in elm.attrib:
+            h = float(elm.attrib['Height'])
+        elif self.grid is not None and 'Height' in self.grid.attrib:
+            h = float(self.grid.attrib['Height'])
+
+        return (w, h)
+
+    def _set_penbrush(self, pathitem, elm):
+        if 'Fill' in elm.attrib:
+            pathitem.setBrush(QBrush(QColor(elm.attrib['Fill'])))
+
+        if 'Stroke' in elm.attrib:
+            pen = QPen(QColor(elm.attrib['Stroke']))
+            pen.setJoinStyle(Qt.MiterJoin)
+            if 'StrokeThickness' in elm.attrib:
+                pen.setWidthF(float(elm.attrib['StrokeThickness']))
+            pathitem.setPen(pen)
+
+    def create_arc(self, elm):
+        # FIXME
+        path = QPainterPath()
+        (w, h) = self._elm_wh(elm)
+        path.addEllipse(0.0, 0.0, w, h)
+
+        thickness = 1
+        if 'ArcThickness' in elm.attrib:
+            thickness = float(elm.attrib['ArcThickness'])
+            path.translate(thickness/2, thickness/2)
+
+        pathitem = self.scene.addPath(path)
+
+        pen = QPen()
+        if 'Fill' in elm.attrib:
+            pen.setColor(QColor(elm.attrib['Fill']))
+        pen.setWidthF(thickness)
+        pen.setCapStyle(Qt.FlatCap)
+        pathitem.setPen(pen)
+
+        return path
+
+    def create_ellipse(self, elm):
+        path = QPainterPath()
+        path.addEllipse(0, 0,
+                        float(elm.attrib['Width']),
+                        float(elm.attrib['Height']))
+        pathitem = self.scene.addPath(path, QPen(Qt.NoPen))
+
+        self._set_penbrush(pathitem, elm)
+
+        margin = self._margin(elm)
+        if margin:
+            pathitem.setPos(margin[0], margin[1])
+
+        return path
+
+    def regular_polygon_path(self, elm):
+        height = float(elm.attrib['Height'])
+        width = float(elm.attrib['Width'])
+        count = int(elm.attrib['PointCount'])
+        path = QPainterPath()
+        (x,y) = (0.0, height/2.0)
+        path.moveTo(x,y)
+        angle = pi/180.0 * 360.0 / count
+        for _ in range(count-1):
+            (x, y) = (x*cos(angle) - y*sin(angle),
+                      x*sin(angle) + y*cos(angle))
+            path.lineTo(x,y)
+        path.closeSubpath()
+
+        path.translate(width/2.0, height/2.0)
+        return path
+
+    def create_path(self, elm):
+        data = elm.attrib['Data']
+        path = self.builder.parse(data)
+        rect = path.boundingRect()
+        path.translate(-rect.x(), -rect.y())
+        pathitem = self.scene.addPath(path, QPen(Qt.NoPen))
+
+        self._set_penbrush(pathitem, elm)
+
+        margin = self._margin(elm)
+        if margin:
+            pathitem.moveBy(margin[0], margin[1])
+
+        if 'VerticalAlignment' in elm.attrib and \
+           'Center' == elm.attrib['VerticalAlignment']:
+            #FIXME
+            gh = float(self.grid.attrib['Height'])
+            pathitem.moveBy(0, gh/2 - rect.height()/2)
+            pathitem.moveBy(-margin[2], -margin[3])
+
+        (w,h) = self._elm_wh(elm)
+        if w is not None and h is not None:
+            trans = pathitem.transform()
+            trans = trans.scale(float(w) / float(rect.width()),
+                                float(h) / float(rect.height()))
+            pathitem.setTransform(trans)
+        return path
+
+    def create_regular_polygon(self, elm):
+        path = self.regular_polygon_path(elm)
+        pathitem = self.scene.addPath(path, QPen(Qt.NoPen))
+
+        self._set_penbrush(pathitem, elm)
+
+        margin = self._margin(elm)
+        if margin:
+            pathitem.setPos(margin[0], margin[1])
+
+        return path
+
+    def paintEvent(self, event):
+        (w, h) = (self.width(), self.height())
+        brect = self.scene.itemsBoundingRect()
+        (bw, bh) = (brect.width(), brect.height())
+        if self.grid is not None and 'Margin' in self.grid.attrib:
+            m = float(self.grid.attrib['Margin'])
+            bw += m*2
+            bh += m*2
+
+        # alignment to center
+        ratio_w = float(w) / float(bw)
+        ratio_h = float(h) / float(bh)
+        if  ratio_h < ratio_w:
+            new_bw = w / ratio_h
+            brect = QRectF(-(new_bw - bw)/2, 0, new_bw, bh)
+        else:
+            new_bh = h / ratio_w
+            brect = QRectF(0, -(new_bh - bh)/2, bw, new_bh)
+
+        self.scene.setSceneRect(brect)
+
+        painter = QPainter(self)
+        painter.setRenderHints(QPainter.Antialiasing)
+        self.scene.render(painter)
+
+def main():
+    import sys
+    app = QApplication(sys.argv)
+
+    widget = MainWidget()
+    widget.show()
+
+    ret = app.exec_()
+    sys.exit(ret)
+
+if __name__ == '__main__':
+    main()
---
+++ ui/slotitem_parser.py
@@ -0,0 +1,187 @@
+
+import re
+
+class ParseError(Exception):
+    """Path data parse error."""
+    def __init__(self, msg, data):
+        self.msg = msg
+        self.data = data
+
+    def __str__(self):
+        return '{0}: {1}'.format(self.msg, self.data)
+
+def num_parser(data):
+    nump = r'^ *(-?[0-9]+\.[0-9]*|-?\.[0-9]+|-?[0-9]+)'
+    m = re.match(nump, data)
+    if m is None:
+        raise ParseError('num_parser', data)
+    return (float(m.group(1)), data[len(m.group(0)):])
+
+def cmd_parser(data):
+    p = r'^ *([a-zA-Z])'
+    m = re.match(p, data)
+    if m is None:
+        raise ParseError('cmd_parser', data)
+    return (m.group(1).lower(), data[len(m.group(0)):])
+
+def point_parser(data):
+    nump = r'(-?[0-9]+\.[0-9]*|-?\.[0-9]+|-?[0-9]+)'
+    p = r'^ *{nump}\s*,?\s*{nump}'.format(nump=nump)
+    m = re.match(p, data)
+    if m is None:
+        raise ParseError('point_parser', data)
+    return ((float(m.group(1)),float(m.group(2))) , data[len(m.group(0)):])
+
+from PyQt5.QtGui import (QBrush, QColor, QFont, QLinearGradient, QPainter,
+        QPainterPath, QPalette, QPen)
+
+class PathBuilder(object):
+    def __init__(self):
+        super(PathBuilder, self).__init__()
+
+    def cmd_m(self, path, data):
+        ((x,y), data) = point_parser(data)
+        path.moveTo(x, y)
+        return data
+
+    def cmd_l(self, path, data):
+        ((x,y), data) = point_parser(data)
+        path.lineTo(x,y)
+        return data
+
+    def cmd_h(self, path, data):
+        pos = path.currentPosition()
+        (x, data) = num_parser(data)
+        path.lineTo(x, pos.y)
+        return data
+
+    def cmd_v(self, path, data):
+        pos = path.currentPosition()
+        (y, data) = num_parser(data)
+        path.lineTo(pos.x, y)
+        return data
+
+    def cmd_c(self, path, data):
+        ((cx1, cy1), data) = point_parser(data)
+        ((cx2, cy2), data) = point_parser(data)
+        ((ex, ey), data) = point_parser(data)
+        path.cubicTo(cx1,cy1, cx2,cy2, ex,ey)
+        self.last_cp = ((cx2,cy2), (ex,ey))
+        return data
+
+    def cmd_q(self, path, data):
+        ((cx, cy), data) = point_parser(data)
+        ((ex, ey), data) = point_parser(data)
+        path.quadTo(cx,cy, ex,ey)
+        self.last_cp = ((cx,cy), (ex,ey))
+        return data
+
+    def cmd_s(self, path, data):
+        if not self.last_cp:
+            ((cx,cy), (ex, ey)) = self.last_cp
+            (cx1, cy1)  = (ex - cx, ey - cy)
+        else:
+            pos = path.currentPosition()
+            (cx1, cy1) = (pos.x(), pos.y())
+
+        ((cx2, cy2), data) = point_parser(data)
+        ((ex, ey), data) = point_parser(data)
+        path.cubicTo(cx1,cy1, cx2,cy2, ex,ey)
+        self.last_cp = ((cx2,cy2), (ex,ey))
+        return data
+
+    def cmd_t(self, path, data):
+        if not self.last_cp:
+            ((cx,cy), (ex, ey)) = self.last_cp
+            (cx1, cy1)  = (ex - cx, ey - cy)
+        else:
+            pos = path.currentPosition()
+            (cx1, cy1) = (pos.x(), pos.y())
+
+        ((ex, ey), data) = point_parser(data)
+        path.cubicTo(cx1,cy1, ex,ey)
+        self.last_cp = ((cx1,cy1), (ex,ey))
+        return data
+
+
+    def cmd_z(self, path, data):
+        path.closeSubpath()
+        return data
+
+    def parse(self, data):
+        data = data.replace('\r\n', ' ')
+
+        cmds = { 'm': self.cmd_m,
+                 'l': self.cmd_l,
+                 'h': self.cmd_h,
+                 'v': self.cmd_v,
+                 'c': self.cmd_c,
+                 'q': self.cmd_q,
+                 's': self.cmd_s,
+                 't': self.cmd_t,
+                 'z': self.cmd_z}
+
+        path = QPainterPath()
+        last_parser = None
+        last_cp = None # (controlpoint, endpoint) of last Bezier curve
+
+        while(True):
+            while(last_parser is not None):
+                try:
+                    data = last_parser(path, data)
+                except ParseError:
+                    break
+
+            # end of data?
+            if re.match(r'\s*$', data):
+                return path
+
+            (cmd, data) = cmd_parser(data)
+            if not cmd in cmds:
+                raise ParseError('unkonw command: ' + cmd, data)
+            data = cmds[cmd](path, data)
+            if cmd != 'z':
+                last_parser = cmds[cmd]
+
+            if not cmd in ['c','q','s','t']:
+                last_cp = None
+
+def test():
+    assert(('t', 'est') == cmd_parser('  test'))
+    assert(('t', 'est') == cmd_parser('test'))
+    try:
+        cmd_parser('1.23')
+        assert(False)
+    except ParseError as e:
+        assert(e.msg == 'cmd_parser')
+
+    assert(( (1.0, 2.0), '') == point_parser('1.0 2.0'))
+    assert(( (1.0, 2.0), '') == point_parser(' 1.0,2.0'))
+    assert(( (1.0, 2.0), '') == point_parser('1.0 , 2.0'))
+    try:
+        point_parser('M1.0 , 2.0')
+        assert(False)
+    except ParseError as e:
+        assert(e.msg == 'point_parser')
+
+    assert(( (0.3, 0.3), '') == point_parser('.3.3'))
+    assert(( (2.0, 0.3), '') == point_parser('2..3'))
+    assert(( (2.0, 0.3), '') == point_parser('2.,.3'))
+    assert(( (20.0, 30.0), '') == point_parser('20,30'))
+    assert(( (-0.3, -0.3), '') == point_parser('-.3-.3'))
+    assert(( (-2.0, -0.3), '') == point_parser('-2.-.3'))
+    assert(( (-20.0, -30.0), '') == point_parser('-20,-30'))
+    try:
+        point_parser('2...3')
+        assert(False)
+    except ParseError as e:
+        assert(e.msg == 'point_parser')
+
+    assert((2.0, 'test') == num_parser('2.test'))
+    assert((-2.0, 'test') == num_parser('-2.test'))
+    assert((2.0, 'test') == num_parser(' 2.0test'))
+    assert((2.0, ' test') == num_parser(' 2.0 test'))
+    assert((20, ' test') == num_parser(' 20 test'))
+
+if __name__ == '__main__':
+    test()
